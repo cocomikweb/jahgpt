19874c94e8f8889bb1f57f8567ca4c4aecaa0787
Cocomik <cocomikweb@gmail.com>
2023-04-20 03:33:57 -0400

    procfile update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:farigpt
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -4,9 +4,8 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for, session
 
 app = Flask(__name__)
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = "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"
-app.secret_key = "sk-iK1LKJljlkjsLKJLjLJF787898Uiojkljljljlkjlj9kjhhH"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+app.secret_key = "K1LKJljlkjsLKJLjLJF787898Uiojkljljljlkjlj9kjhhH"
 
 
 @app.route("/", methods=("GET", "POST"))
